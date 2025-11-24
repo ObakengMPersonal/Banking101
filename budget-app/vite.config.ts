81fab9f290ba347c6ffbf4ef7be2f13b07b64133
Obakeng Mathibedi <82166973+SlowTyper-sys@users.noreply.github.com>
2025-11-24 20:51:48 +0200

    Dockerizing the App, added services pages and utils, added Auth hook

--- budget-app/vite.config.ts
+++ budget-app/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,7 +8,6 @@ export default defineConfig({
     react({
       babel: {
         plugins: [
-          tailwindcss(),
           ['babel-plugin-react-compiler']],
       },
     }),
